5f285de333312ca911f1bd767884cba4b53e753a
zazk <juandedioz@gmail.com>
2017-09-18 00:20:09 -0500

    Setup API error manage

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.post("/api/auth", (req, res) => {
+    res.status(400).json({ errors: { global: "Invalid Credentials Moron!" } });
+});
+
+app.get("/*", (req, response) => {
+    response.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8900, () => console.log("Running on 8900 Damn Nerd"));
